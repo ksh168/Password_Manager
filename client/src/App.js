916b0af1c6b5bebfa48248316b3fd7adc781de07
Ubuntu <azureuser@test1.xnzkd55asrbutdal3q4ujhdqkh.bx.internal.cloudapp.net>
2021-07-23 18:09:51 +0000

    connection between frontend and backend is now active, all the info filled in the form is now viewable at mysql db

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import { useState } from 'react';
+import { useState } from 'react';//using State
+
+import Axios from 'axios'
 
 
 function App() {
 
-  const 
+  const [password, setPassword] = useState('')// '' -> means it's a string
+  const [title, setTitle] = useState('')
 
+  const addPassword = () => {
+    Axios.post('http://localhost:3001/addpassword', {
+    password: password, title: title,
+    });
+  };
 
   return (
     <div className="App">
       <div className="AddingPassword">
-        <input type="text" placeholder="Ex. thisisatestpassword"/>
-        <input type="text" placeholder="Ex. GitHub"/>
-        <button>Add Password!</button>
+        <input type="text" placeholder="Ex. thisisatestpassword" onChange={(event) => {setPassword(event.target.value)}} />
+        {/* onChange causes it to put whatever value is put in the field by the user whenever there's a change (it's called again when ther's change to the input) */}
+        
+        <input type="text" placeholder="Ex. GitHub" onChange={(event) => {setTitle(event.target.value)}} />
+        
+        <button onClick={addPassword}>Add Password!</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors')
+
 const mysql = require('mysql')
 
 //as react app run on port 3000 usually
 const PORT = 3001;
 
+//using cors to parse json
+app.use(cors());
+app.use(express.json());
 
 //to create connection with mysql db
 const db = mysql.createConnection({
@@ -17,9 +22,28 @@ const db = mysql.createConnection({
 
 
 //adding route
-app.get('/', (req, res) => {
-    res.send("Hello There!");
+// app.get('/', (req, res) => {
+//     res.send("Hello There!");
+// });
+
+//to make the request
+app.post('/addpassword', (req, res) => {
+    //access values from frontend
+    const {password, title} = req.body
+
+    //insert to db
+    db.query("INSERT INTO passwords (password, title) VALUES (?,?)", 
+    [password, title], 
+    (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send("Success");
+        }
+    });
 });
+
 app.listen(PORT, () => {
     console.log("Server is up and running!");
   });
