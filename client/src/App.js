617a64ac3a8f80b7a2aaddc27392ba0c9bf158dd
Ubuntu <azureuser@test1.xnzkd55asrbutdal3q4ujhdqkh.bx.internal.cloudapp.net>
2021-07-23 11:23:18 +0000

    connect mysql db and setup basic react frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AddingPassword">
+        <input type="text" placeholder="Ex. thisisatestpassword"/>
+        <input type="text" placeholder="Ex. GitHub"/>
+        <button>Add Password!</button>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+
+const mysql = require('mysql')
+
+//as react app run on port 3000 usually
 const PORT = 3001;
+
+
+//to create connection with mysql db
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password123',
+    database: 'PasswordManager',
+});
 
 
 //adding route
@@ -10,3 +23,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log("Server is up and running!");
   });
+
+
+// CREATE TABLE `PasswordManager`.`passwords`(`id` INT NOT NULL AUTO_INCREMENT,`password` VARCHAR(255) NOT NULL,
+// `title` VARCHAR(255) NOT NULL,PRIMARY KEY (`id`));
